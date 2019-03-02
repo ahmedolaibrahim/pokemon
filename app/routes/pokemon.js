2f6dece209af28a55a0af871f1238b512b9e3049
Ibrahim Ahmed <ahmedolaibrahim@gmail.com>
2019-03-02 11:37:29 +0100

    added pokemon routes

--- app/routes/pokemon.js
+++ app/routes/pokemon.js
@@ -1,10 +1,27 @@
+/**
+ * Module dependencies
+ */
 import { Router } from 'express';
 import { asyncErrorHandler } from '../utils/middleware/errorHandler';
 
+/**
+ * Module variables
+ */
 const pokemonRouter =  Router();
 
-pokemonRouter.get('/',asyncErrorHandler(async (req, re, next) => {
-    res.send("pokemon route goes here");
+/**
+ * Pokemon Routes 
+ */
+pokemonRouter.get('/',asyncErrorHandler(async (req, res, next) => {
+    res.send("returns list of all pokemons");
+}));
+
+pokemonRouter.get('/name/:name',asyncErrorHandler(async (req, res, next) => {
+  res.send("get pokemon by name");
+}));
+
+pokemonRouter.get('/compare',asyncErrorHandler(async (req, res, next) => {
+  res.send("compare list of pokemons");
 }));
 
 
